2d4f1fb05c9ee414166f41a9b5b5038f67ca4b26
AlexShchehlov <97538084+AlexShchehlov@users.noreply.github.com>
2022-05-10 13:25:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+console.log('Я учу JavaScript');
+ 
+let userName = 'Alex';
+let userAge = '40';
+let userColorEyes = 'Grey';
+userName = 'Oleksandr';
+userColorEyes = 'Green';
+let myNewAge = '21';
+userAge = myNewAge
+
+console.log(userColorEyes);
+console.log(userName);
+console.log(userAge);
+ 
